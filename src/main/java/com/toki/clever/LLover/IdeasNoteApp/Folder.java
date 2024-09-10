d76f2f39551a4296ece28e4d585db11ec47d2f62
Trever Fuhrer <clevertrever31@gmail.com>
2024-09-09 23:58:48 -0500

    Setup project

--- src/main/java/com/toki/clever/LLover/IdeasNoteApp/Folder.java
+++
@@ -1,6 +0,0 @@
-package com.toki.clever.LLover.IdeasNoteApp;
-
-public class Folder {
-
-    //private
-}
--- src/main/java/com/toki/clever/commands/methods/MLBCommand/MLBUpdates.java
+++
@@ -1,6 +0,0 @@
-package com.toki.clever.commands.methods.MLBCommand;
-
-public class MLBUpdates {
-
-
-}
